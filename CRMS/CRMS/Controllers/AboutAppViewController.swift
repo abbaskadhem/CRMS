71ea3f9fc33e8556465b929c29f45a7fedd231b4
MaryamTalal105 <202302082@student.polytechnic.bh>
2025-12-18 12:38:56 +0300

    settings UI phase 1

---
+++ CRMS/CRMS/Controllers/AboutAppViewController.swift
@@ -0,0 +1,8 @@
+//
+//  AboutAppViewController.swift
+//  CRMS
+//
+//  Created by BP-36-201-04 on 18/12/2025.
+//
+
+import Foundation
---
+++ CRMS/CRMS/Controllers/SettingsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsViewController.swift
+//  CRMS
+//
+//  Created by BP-36-201-04 on 18/12/2025.
+//
+
+import Foundation
