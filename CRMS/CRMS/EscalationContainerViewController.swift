bdea6f13c79dfc034e3ee7add7a33a29ff56c2ff
HoorHasan30 <202301820@student.polytechnic.bh>
2025-12-22 12:41:10 +0300

    Started with analysis

---
+++ CRMS/CRMS/EscalationContainerViewController.swift
@@ -0,0 +1,36 @@
+//
+//  EscalationContainerViewController.swift
+//  CRMS
+//
+//  Created by BP-19-130-05 on 22/12/2025.
+//
+
+import UIKit
+
+class EscalationContainerViewController: UIViewController {
+
+    
+    @IBOutlet weak var totalNum: UILabel!
+    @IBOutlet weak var escalatedNum: UILabel!
+    
+    @IBOutlet weak var pieChart: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
